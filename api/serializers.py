bd2fac6befd6259190ee65c5b09d337b1aa450a5
mohammedi <hm@big-mama.io>
2018-11-30 19:42:40 +0100

    feat[serializers]: write serializers for Period and Document models

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Dictionary, Entry, Meaning
+from api.models import Dictionary, Entry, Meaning, Period, Document
 
 
 # Serializers define the API representation.
@@ -8,12 +8,26 @@ class MeaningSerializer(serializers.ModelSerializer):
         model = Meaning
         fields = '__all__'
 
+
 class EntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entry
         fields = ['term', 'meaning_set']
 
+
 class DictionarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Dictionary
         fields = ['name', 'entry_set']
+
+
+class PeriodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Period
+        fields = '__all__'
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
