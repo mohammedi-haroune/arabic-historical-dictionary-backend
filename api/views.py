a677a5700254085f67fa6c5e1ac4509981a62451
mohammedi <hm@big-mama.io>
2018-11-30 19:43:08 +0100

    feat[views]: write views for Period, Category and Document models

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from api.serializers import DictionarySerializer, EntrySerializer, MeaningSerializer
-from api.models import Dictionary, Entry, Meaning
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.models import Dictionary, Entry, Meaning, Period, Document
+from api.serializers import DictionarySerializer, EntrySerializer, MeaningSerializer, PeriodSerializer, \
+    DocumentSerializer
+
+
 # Create your views here.
 
 
@@ -9,6 +14,7 @@ from api.models import Dictionary, Entry, Meaning
 class DictionaryViewSet(viewsets.ModelViewSet):
     queryset = Dictionary.objects.all()
     serializer_class = DictionarySerializer
+
 
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
@@ -18,3 +24,48 @@ class EntryViewSet(viewsets.ModelViewSet):
 class MeaningViewSet(viewsets.ModelViewSet):
     queryset = Meaning.objects.all()
     serializer_class = MeaningSerializer
+
+
+class PeriodViewSet(viewsets.ModelViewSet):
+    queryset = Period.objects.all()
+    serializer_class = PeriodSerializer
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    serializer_class = DocumentSerializer
+    queryset = Document.objects.all()
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Document.objects.all()
+        categories = self.request.query_params.getlist('categories[]', [])
+        periods = self.request.query_params.getlist('periods[]', [])
+        print(queryset)
+        print(categories)
+        print(periods)
+        if categories:
+            queryset = queryset.filter(category__in=categories)
+
+        if periods:
+            queryset = queryset.filter(period_id__in=periods)
+        return queryset
+
+
+
+
+
+class CategoryList(APIView):
+    """
+    View to list all users in the system.
+
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        categories = set([doc.category for doc in Document.objects.all()])
+        return Response(categories)
