30d2f025cfe3ed813457f9f6adb6266088d29db4
ressay <bourahla.yasser@gmail.com>
2018-12-07 18:36:54 +0100

    added path test to load corpus

--- api/corpus/initializer.py
+++ api/corpus/initializer.py
@@ -4,5 +4,7 @@ from pathlib import Path
 from .HistoricalCorpus import HistoricalCorpus
 home = str(Path.home())
 xmlDir = os.environ.get('XML_CORPUS_DIR', home + '/xmlCorpus') # where to put xml files
-corpus = HistoricalCorpus(xmlDir)
+corpus = None
+if os.path.isdir(xmlDir):
+    corpus = HistoricalCorpus(xmlDir)
 
