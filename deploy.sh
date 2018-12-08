b920c47e6e87e9298d8b933168cf32effb4d43c8
mohammedi <hm@big-mama.io>
2018-12-08 20:49:38 +0100

    fix[deploy.sh]: add secrets creation

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 docker build -t gcr.io/arabic-historical-dictionary/tal-backend .
 docker push gcr.io/arabic-historical-dictionary/tal-backend
+kubectl create secret generic cloudsql-oauth-credentials --from-file=credentials.json
+kubectl create secret generic cloudsql --from-literal=username=tal --from-literal=password=tal
 kubectl delete -f tal.yaml
 kubectl create -f tal.yaml
