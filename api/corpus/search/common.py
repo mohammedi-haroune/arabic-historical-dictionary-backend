2ba1959335c317b3e733d3b8db9eb7781dff7d11
MOHAMMEDI Haroune <mohammedi.haroun@gmail.com>
2018-12-27 08:24:43 +0100

    fix[common]: instantiate elasticsearch connection once and reuse it if possible

--- api/corpus/search/common.py
+++ api/corpus/search/common.py
@@ -1,17 +1,20 @@
 from elasticsearch import Elasticsearch
 
-
+_es = None
 
 def connect_elasticsearch():
+    global _es
     port = 9200
     ip_address = '35.226.252.137'
-    _es = Elasticsearch([{'host': ip_address , 'port': port}], timeout=30)
-    if _es.ping():
-        print('INFO ELASTIC: CONNECTED')
+    if _es is not None:
+        return _es
     else:
-        print('INFO ELASTIC: COULD NOT CONNECT')
-
-    return _es
+        _es = Elasticsearch([{'host': ip_address , 'port': port}], timeout=60)
+        if _es.ping():
+            print('INFO ELASTIC: CONNECTED')
+        else:
+            print('INFO ELASTIC: COULD NOT CONNECT')
+        return _es
 
 def get_sentence_index_name(lemma):
     if lemma:
